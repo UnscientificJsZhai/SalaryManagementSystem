6ddeb498b803bbefee93580c78691c3c55ffd9ea
MikasaStacy <63218646+Mikasa-777@users.noreply.github.com>
2021-12-04 10:14:03 +0800

    修改登录界面

--- src/main/java/cn/edu/nwpu/salarymanagementsystem/controller/SalarySystem.java
+++ src/main/java/cn/edu/nwpu/salarymanagementsystem/controller/SalarySystem.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -30,19 +32,34 @@ public class SalarySystem {
     /**
      * 登录请求
      *
-     * @param Id 用户ID
+     * @param id       用户ID
      * @param password
      * @param session
      */
     @RequestMapping("/login")
-    public String processLogin(@RequestParam(value = "Id", defaultValue = "") Long Id,
-                               @RequestParam(value = "password", defaultValue = "") String password, HttpSession session) {
-
-        if (administratorService.login(Id, password)) {
-            session.setAttribute("administrator", Id);
+    public String processLogin(@RequestParam(value = "id", defaultValue = "") Long id,
+                               @RequestParam(value = "password", defaultValue = "") String password,
+                               HttpSession session, HttpServletResponse res) {
+        //TODO 无法实现自由选择是否保存，先写成全部保留的方式
+        if (administratorService.login(id, password)) {
+            session.setAttribute("administrator", id);
+            Cookie cookie1 = new Cookie("name", id.toString());
+            cookie1.setMaxAge(24 * 60 * 60);
+            res.addCookie(cookie1);
+            Cookie cookie2 = new Cookie("password", password);
+            cookie1.setMaxAge(24 * 60 * 60);
+            res.addCookie(cookie2);
+            res.addCookie(cookie2);
             return "redirect:/Admin/AdminView";
-        } else if (staffService.login(Id, password)) {
-            session.setAttribute("staff", Id);
+        } else if (staffService.login(id, password)) {
+            session.setAttribute("staff", id);
+            Cookie cookie1 = new Cookie("name", id.toString());
+            cookie1.setMaxAge(24 * 60 * 60);
+            res.addCookie(cookie1);
+            Cookie cookie2 = new Cookie("password", password);
+            cookie1.setMaxAge(24 * 60 * 60);
+            res.addCookie(cookie2);
+            res.addCookie(cookie2);
             return "redirect:/Staff/ShowInfo";
         } else {
             return "../Login";
--- src/main/webapp/WEB-INF/test1/sign-in.jsp
+++ src/main/webapp/WEB-INF/test1/sign-in.jsp
@@ -43,24 +43,24 @@
 <body class="text-center">
 
 <main class="form-signin">
-    <form>
+    <form action="/login" method="post">
         <img class="mb-4" src="${pageContext.request.contextPath}/resources/css/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div class="form-floating">
-            <input name="" type="number" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input name="id" value="${cookie.name.value}" type="number" class="form-control" id="floatingInput" placeholder="name@example.com" >
             <label for="floatingInput">Id</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input name="password" value="${cookie.password.value}" type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
         </div>
 
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
+<%--        <div class="checkbox mb-3">--%>
+<%--            <label>--%>
+<%--                <input name="remember" type="checkbox" value="remember-me"> Remember me--%>
+<%--            </label>--%>
+<%--        </div>--%>
         <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
     </form>
