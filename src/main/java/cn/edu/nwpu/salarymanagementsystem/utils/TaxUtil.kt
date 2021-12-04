4eb72c1e018629ae40a4407ed203e32b743eccbb
UnscientificJsZhai <1772316174@qq.com>
2021-12-05 03:07:43 +0800

    更改个税计算

--- src/main/java/cn/edu/nwpu/salarymanagementsystem/utils/TaxUtil.kt
+++ src/main/java/cn/edu/nwpu/salarymanagementsystem/utils/TaxUtil.kt
@@ -10,7 +10,7 @@ import cn.edu.nwpu.salarymanagementsystem.pojo.data.salary.Salary
  * @receiver 薪水列表。
  * @return 计算出的个税数额。
  */
-fun List<Salary>.calculateTax(): Double {
+private fun List<Salary>.calculateTax(): Double {
     var total = 0.0
     for (salaryItem in this) {
         total += (salaryItem.total() - 5000).coerceAtLeast(0.0)
@@ -25,4 +25,34 @@ fun List<Salary>.calculateTax(): Double {
         total <= 960000 -> (total - 660000) * 0.35 + 85920
         else -> (total - 960000) * 0.45 + 181920
     }
+}
+
+private val comparator = Comparator<Salary> { o1, o2 -> o1.month - o2.month }
+
+/**
+ * 按年份分别计算不同年的个税。
+ *
+ * @receiver 所有工资信息。
+ * @return 年份和个税数目的配对列表。
+ */
+fun List<Salary>.totalTax(): List<Pair<Int, Double>> {
+    this.sortedWith(comparator)
+    val result = ArrayList<Pair<Int, Double>>()
+    val maxMonth = maxOf {
+        it.month
+    }
+    val maxYear = maxMonth / 12 + if (maxMonth % 12 == 0) 0 else 1
+    val sublist = ArrayList<Salary>()
+    for (year in 1..maxYear) {
+        sublist.clear()
+        for (salary in this) {
+            if (salary.month > year * 12) {
+                break
+            } else if (salary.month > year * 12 - 12) {
+                sublist.add(salary)
+            }
+        }
+        result.add(Pair(year + 1999, sublist.calculateTax()))
+    }
+    return result
 }
--- src/test/java/cn/edu/nwpu/salarymanagementsystem/service/CalculateTaxTest.kt
+++ src/test/java/cn/edu/nwpu/salarymanagementsystem/service/CalculateTaxTest.kt
@@ -2,6 +2,7 @@ package cn.edu.nwpu.salarymanagementsystem.service
 
 import cn.edu.nwpu.salarymanagementsystem.pojo.data.salary.Salary
 import cn.edu.nwpu.salarymanagementsystem.utils.calculateTax
+import cn.edu.nwpu.salarymanagementsystem.utils.totalTax
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -42,5 +43,24 @@ class CalculateTaxTest {
         assertEquals(90.0 * 12, 8000.perMonth().calculateTax(), 0.01)
         assertEquals(7320.0, 12000.perMonth().calculateTax(), 0.01)
         assertEquals(12120.0, 16000.perMonth().calculateTax(), 0.01)
+    }
+
+    @Test
+    fun taxPerYearTest() {
+        val year1 = StubSalary(month = 1, total = 50000.0)
+        val year2 = StubSalary(month = 13, total = 50000.0)
+        val year3 = StubSalary(month = 25, total = 50000.0)
+        val year4 = StubSalary(month = 37, total = 50000.0)
+        val result = arrayListOf(year1, year2, year3, year4).totalTax()
+
+        assertEquals(2000, result[0].first)
+        assertEquals(2001, result[1].first)
+        assertEquals(2002, result[2].first)
+        assertEquals(2003, result[3].first)
+
+        assertEquals(3420.0, result[0].second, 0.01)
+        assertEquals(3420.0, result[1].second, 0.01)
+        assertEquals(3420.0, result[2].second, 0.01)
+        assertEquals(3420.0, result[3].second, 0.01)
     }
 }
