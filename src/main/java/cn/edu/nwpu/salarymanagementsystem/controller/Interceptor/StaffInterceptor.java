c10c84b2fe2a74b23e56832364b7cd63b0f06447
UnscientificJsZhai <1772316174@qq.com>
2021-12-05 01:22:46 +0800

    修正注释

--- src/main/java/cn/edu/nwpu/salarymanagementsystem/controller/Interceptor/StaffInterceptor.java
+++ src/main/java/cn/edu/nwpu/salarymanagementsystem/controller/Interceptor/StaffInterceptor.java
@@ -1,5 +1,6 @@
 package cn.edu.nwpu.salarymanagementsystem.controller.Interceptor;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,17 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * @ClassName StaffInterceptor
- * @Description 员工访问拦截器
+ * 员工相关URL拦截器。
+ *
  * @Author xuLyi
- * @Date 2021/12/1
- * @Version 1.0
  */
 public class StaffInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object arg2) throws Exception {
         HttpSession session = request.getSession();
-        //如果session中没有管理员信息，或者信息不对，则跳转到登录界面
+        // 如果session中没有管理员信息，或者信息不对，则跳转到登录界面
         if (session.getAttribute("staff") == null) {
             request.setAttribute("message", "You don't have permission.");
             request.getRequestDispatcher("/WEB-INF/test1/sign-in.jsp").forward(request, response);
@@ -28,12 +27,11 @@ public class StaffInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object obj, ModelAndView model)
-            throws Exception {
+    public void postHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object obj, ModelAndView model) {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object obj,
-                                Exception exception) throws Exception {
+    public void afterCompletion(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object obj,
+                                Exception exception) {
     }
 }
