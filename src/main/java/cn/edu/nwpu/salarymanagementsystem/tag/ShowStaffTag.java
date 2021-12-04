2fe7643758ab3bc2512c9b471320ab6991983677
MikasaStacy <63218646+Mikasa-777@users.noreply.github.com>
2021-12-04 20:19:58 +0800

    员工列表标签实现

--- src/main/java/cn/edu/nwpu/salarymanagementsystem/tag/ShowStaffTag.java
+++ src/main/java/cn/edu/nwpu/salarymanagementsystem/tag/ShowStaffTag.java
@@ -1,11 +1,22 @@
 package cn.edu.nwpu.salarymanagementsystem.tag;
 
+import cn.edu.nwpu.salarymanagementsystem.pojo.data.staff.MutableStaff;
+import cn.edu.nwpu.salarymanagementsystem.pojo.data.staff.Staff;
+import kotlin.Pair;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class showStaffTag extends SimpleTagSupport {
+public class ShowStaffTag extends SimpleTagSupport {
+
+    private ArrayList<Pair<MutableStaff,String>> staffPairList;
+
+    public void setStaffPairList(ArrayList<Pair<MutableStaff,String>> staffPairList){
+        this.staffPairList = staffPairList;}
+
     public void doTag() throws JspException, IOException {
         JspWriter out = getJspContext().getOut();
 
@@ -17,12 +28,14 @@ public class showStaffTag extends SimpleTagSupport {
                         "        <th>部门名称</th>\n" +
                         "        <th>操作</th>\n" +
                         "        </tr>\n");
-        for(int i = 0;i<;i++){
+        for(int i = 0;i<staffPairList.size();i++){
+            Staff staff = staffPairList.get(i).getFirst();
+            String departmentName = staffPairList.get(i).getSecond();
             outPrint.append("<tr>\n" +
-                    "<td>" +  + "</td>\n" +
-                    "<td>" +  + "</td>\n" +
-                    "<td>" +  + "</td>\n" +
-                    "<td>" +  + "</td>\n" +
+                    "<td>" +staff.getId() + "</td>\n" +
+                    "<td>" + staff.getName() + "</td>\n" +
+                    "<td>" + staff.getDepartment() + "</td>\n" +
+                    "<td>" + departmentName + "</td>\n" +
                     "<td><a href=\"<c:url value=\"/Admin/searchStaff?sid=${staff.id}\"/>\">查看信息</a></td>" +
                     "<td><a name=\"del\" href=\"#?sid=${staff.id}\">删除</a></td>" +
                     "</tr>");
--- src/main/webapp/WEB-INF/test1/staff-info.jsp
+++ src/main/webapp/WEB-INF/test1/staff-info.jsp
@@ -7,6 +7,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="showStaffTag" uri="showStaffTag" %>
 <html>
 <head>
     <title>Title</title>
@@ -46,7 +47,7 @@
     <h2>Section title</h2>
     <div class="table-responsive">
         <table class="table table-striped table-sm">
-
+            <showStaffTag:showStaff staffPairList="${staffList}"/>
         </table>
     </div>
 </div>
